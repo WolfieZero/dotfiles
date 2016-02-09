48fcfab55c9a6c92fe9a70f6df29dfc2e5176195
WolfieZero <neil@wolfiezero.com>
2016-02-09 09:07:17 +0000

    remove neovim stuffs

--- install/vim.sh
+++ install/vim.sh
@@ -15,16 +15,6 @@ ln -s ${HOME}/dotfiles/vim ${HOME}/.vim # vim folder
 ln -s ${HOME}/dotfiles/vim/.vimrc ${HOME}/.vimrc # vim file
 ln -s ${HOME}/dotfiles/vim/.gvimrc ${HOME}/.gvimrc # vim file
 
-echo "Remove Neovim configs"
-#rm -rf ${HOME}/.config/nvim
-
-echo "Make the config directory"
-mkdir ${HOME}/.config
-
-#echo "Link Neovim to Vim configs"
-#ln -s ${HOME}/dotfiles/vim ${HOME}/.config/nvim # vim folder
-#ln -s ${HOME}/dotfiles/vim/.vimrc ${HOME}/dotfiles/vim/init.vim # vim file
-
 echo "Download and install bundles"
 git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/dotfiles/vim/bundle/Vundle.vim
 vim +PluginInstall +qall
