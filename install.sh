301cda320882670f40675250520a8f54056d81d3
Neil Sweeney <neil@Neils-MacBook-Pro.local>
2016-09-11 00:37:02 +0100

    update the install order

--- install.sh
+++ install.sh
@@ -55,16 +55,6 @@ notice_end
 source install/osx.sh
 
 notice_start
-echo "Install powerline fonts"
-notice_end
-git clone https://github.com/powerline/fonts.git $HOME/dotfiles/powerline-fonts
-cd $HOME/dotfiles/powerline-fonts
-./install.sh
-cd ..
-rm -rf powerline-fonts
-cd ${HOME}
-
-notice_start
 echo "Symlink files & folders"
 notice_end
 rm -f ${HOME}/.jscsrc
@@ -86,5 +76,15 @@ notice_start
 echo "Setup Oh My ZSH"
 notice_end
 source install/oh-my-zsh.sh
+
+notice_start
+echo "Install powerline fonts"
+notice_end
+git clone https://github.com/powerline/fonts.git $HOME/dotfiles/powerline-fonts
+cd $HOME/dotfiles/powerline-fonts
+./install.sh
+cd ..
+rm -rf powerline-fonts
+cd ${HOME}
 
 echo "Done."
