2e72e26b22ebdebcad754a8a37344415605f175e
Neil Sweeney <neil@wolfiezero.com>
2016-09-23 20:26:07 +0100

    removed taps

--- install/brew.sh
+++ install/brew.sh
@@ -11,11 +11,6 @@ brew update
 # Upgrade any already-installed formulae
 brew upgrade
 
-# Tap installers
-brew tap homebrew/dupes
-brew tap homebrew/versions
-brew tap josegonzalez/homebrew-php
-
 # Install GNU core utilities (those that come with OS X are outdated)
 #brew install coreutils
 #echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
@@ -34,9 +29,8 @@ brew install zsh
 # Install PHP with MariaDB
 brew install homebrew/dupes/grep
 brew install mariadb
-brew install php70 --with-mysql --without-apache --with-fpm
-brew install php70-imagick php70-mcrypt php70-xdebug
-brew install josegonzalez/php/composer
+brew install homebrew/php/php70 --with-mysql --without-apache --with-fpm
+brew install homebrew/php/php70-imagick homebrew/php/php70-mcrypt homebrew/php/php70-xdebug
 
 # Post-Install stuffs
 brew services start mariadb
