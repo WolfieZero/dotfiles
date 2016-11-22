67f786b5fb42e7c806d623dccedd840e10b8223d
Neil Sweeney <neil@wolfiezero.com>
2016-11-22 11:48:30 +0000

    added ssh link

--- install/8--symlinks.sh
+++ install/8--symlinks.sh
@@ -3,7 +3,9 @@
 # Symlinks
 # ==============================================================================
 
+rm -f ${HOME}/.ssh
 rm -f ${HOME}/.jscsrc
 rm -f ${HOME}/.jshintrc
+ln -s ${HOME}/Dropbox/syslocal/ssh ${HOME}/.ssh
 ln -s ${HOME}/dotfiles/jscsrc ${HOME}/.jscsrc
 ln -s ${HOME}/dotfiles/jshintrc ${HOME}/.jshintrc
