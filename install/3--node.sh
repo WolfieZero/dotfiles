f17174accf4778cbbf860556401ae85216a5b93c
Neil Sweeney <neil@wolfiezero.com>
2017-02-28 21:00:35 +0000

    updated nvm download

--- install/3--node.sh
+++ install/3--node.sh
@@ -8,7 +8,7 @@
 # ==============================================================================
 
 if test ! $(which nvm); then
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 fi
 
 
