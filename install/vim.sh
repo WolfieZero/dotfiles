209e9a613e39106da0b9efd3a1f052f2a4739195
WolfieZero <neil@wolfiezero.com>
2018-01-29 17:11:14 +0000

    Clear out vim configs

--- install/vim.sh
+++ install/vim.sh
@@ -8,21 +8,19 @@ brew install macvim
 brew install vim
 
 
-# Removing any existing Vim configs
+# Removing any existing Vim configs and update
 # ==============================================================================
 
 rm -rf ${HOME}/.vim
+ln -s ${HOME}/dotfiles/.vim ${HOME}/.vim
+
 rm -rf ${HOME}/.vimrc
+ln -s ${HOME}/dotfiles/.vimrc ${HOME}/.vimrc
+
 rm -rf ${HOME}/.gvimrc
-rm -rf ${HOME}/dotfiles/.vim/bundle
+ln -s ${HOME}/dotfiles/.gvimrc ${HOME}/.gvimrc
 
-
-# Link Vim configs to dotfiles
-# ==============================================================================
-
-ln -s ${HOME}/dotfiles/.vim ${HOME}/.vim # vim folder
-ln -s ${HOME}/dotfiles/.vimrc ${HOME}/.vimrc # vim file
-ln -s ${HOME}/dotfiles/.gvimrc ${HOME}/.gvimrc # vim file
+rm -rf ${HOME}/dotfiles/vim/bundle
 
 
 # Download and install bundles
