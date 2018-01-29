0e68845d997ba12e62b449f6c981fa29c253371d
WolfieZero <neil@wolfiezero.com>
2018-01-29 17:10:54 +0000

    Update file names to be dot based

--- install/8--symlinks.sh
+++ install/8--symlinks.sh
@@ -7,10 +7,10 @@ rm -f ${HOME}/.ssh
 ln -s ${HOME}/Dropbox/syslocal/ssh ${HOME}/.ssh
 
 rm -f ${HOME}/.php_cs
-ln -s ${HOME}/dotfiles/php_cs ${HOME}/.php_cs
+ln -s ${HOME}/dotfiles/.php_cs ${HOME}/.php_cs
 
 rm -f ${HOME}/.jscsrc
-ln -s ${HOME}/dotfiles/jscsrc ${HOME}/.jscsrc
+ln -s ${HOME}/dotfiles/.jscsrc ${HOME}/.jscsrc
 
 rm -f ${HOME}/.jshintrc
-ln -s ${HOME}/dotfiles/jshintrc ${HOME}/.jshintrc
+ln -s ${HOME}/dotfiles/.jshintrc ${HOME}/.jshintrc
