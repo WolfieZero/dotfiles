825ae7356395d713fde6b0c76077cec07d442807
Neil Sweeney <neil@wolfiezero.com>
2021-06-05 18:53:52 +0100

    Add fonts installer

---
+++ setup-new-mac/install-fonts.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Install Apps
+# ------------------------------------------------------------------------------
+
+source ~/dotfiles/setup-new-mac/_config.sh
+
+brew tap homebrew/cask-fonts
+brew install --cask font-victor-mono
